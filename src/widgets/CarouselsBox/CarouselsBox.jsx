afd66733880bc17513e754c70bcf31c012ae2518
OlgaTabisheva <oliatabisheva@gmail.com>
2024-11-26 09:53:50 +0100

    update

--- src/widgets/CarouselsBox/CarouselsBox.jsx
+++ src/widgets/CarouselsBox/CarouselsBox.jsx
@@ -1,16 +1,20 @@
-import style from './CarouselsBox.module.scss'
-import {useCallback, useRef} from "react";
+import style from "./CarouselsBox.module.scss";
+import { useCallback, useRef } from "react";
 import RecipeCard from "../RecipeCard/RecipeCard.jsx";
-import {Pagination, Navigation} from 'swiper/modules';
-import {Swiper, SwiperSlide} from "swiper/react";
-import 'swiper/scss';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import { Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/scss";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import ButtonPicture from "../../shared/Buttons/ButtonPicture/ButtonPicture.jsx";
 
-
-function CarouselsBox({instantNewRecipes, carouselTitle, instantLikes, setInstantLikes, isAuthenticated}) {
-
+function CarouselsBox({
+  instantNewRecipes,
+  carouselTitle,
+  instantLikes,
+  setInstantLikes,
+  isAuthenticated,
+}) {
   const sliderRef = useRef(null);
 
   const handlePrev = useCallback(() => {
@@ -22,7 +26,6 @@ function CarouselsBox({instantNewRecipes, carouselTitle, instantLikes, setInstan
     if (!sliderRef.current) return;
     sliderRef.current.swiper.slideNext();
   }, []);
-
 
   return (
     <div className={style.carouselsBox}>
@@ -36,37 +39,53 @@ function CarouselsBox({instantNewRecipes, carouselTitle, instantLikes, setInstan
                 slidesPerView: 1,
               },
               600: {
-                slidesPerView: 1,
+                slidesPerView: 2,
               },
-              1010: {
+              900: {
                 slidesPerView: 3,
-              }
+              },
             }}
             centeredSlides={false}
             navigation={false}
             spaceBetween={30}
-            pagination={{type: 'fraction'}}
+            pagination={{ type: "fraction" }}
             ref={sliderRef}
             modules={[Pagination, Navigation]}
-            style={{"--swiper-pagination-color": "#fff"}}>
-            {instantNewRecipes && instantNewRecipes?.map((obj, index) => (
-              <SwiperSlide key={index} className={style.carouselsBox__swiperSlide}>
-                <RecipeCard
-                  isBtnEdit={false}
-                  key={obj.id} {...obj}
-                  instantLikes={instantLikes}
-                  setInstantLikes={setInstantLikes}
-                  isAuthenticated={isAuthenticated}/>
-              </SwiperSlide>))}
+            style={{ "--swiper-pagination-color": "#fff" }}
+          >
+            {instantNewRecipes &&
+              instantNewRecipes?.map((obj, index) => (
+                  <SwiperSlide
+                    key={index}
+                    className={style.carouselsBox__swiperSlide}
+                  >
+                    <RecipeCard
+                      isBtnEdit={false}
+                      key={obj.id}
+                      {...obj}
+                      instantLikes={instantLikes}
+                      setInstantLikes={setInstantLikes}
+                      isAuthenticated={isAuthenticated}
+                    />
+                  </SwiperSlide>
+              ))}
           </Swiper>
         </div>
         <div className={style.carouselsBox__buttons}>
-          <ButtonPicture size={'arrowNext'} value={'ArrowMLeft'} onClick={handlePrev}></ButtonPicture>
-          <ButtonPicture size={'arrowPrev'} value={'ArrowMRight'} onClick={handleNext}/>
+          <ButtonPicture
+            size={"arrowNext"}
+            value={"ArrowMLeft"}
+            onClick={handlePrev}
+          ></ButtonPicture>
+          <ButtonPicture
+            size={"arrowPrev"}
+            value={"ArrowMRight"}
+            onClick={handleNext}
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default CarouselsBox
+export default CarouselsBox;
